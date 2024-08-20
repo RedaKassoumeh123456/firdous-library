0e2ea6ba9f26a9a4ced3252223094f90db4d4064
MaenGit <MaenGithub@gmail.com>
2024-08-20 17:43:40 +0300

    first nav bar commit

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,9 +2,21 @@ import Link from "next/link"
 
 const NavBar = () => {
     return (
-        <nav className='bg-primary p-3 pr-5'>
+        <nav className='bg-secondary  p-3 pr-5 h-16 w-full '>
             {/* links */}
-            <div className='links '>
+            <div className='links flex items-center justify-start h-full gap-4'>
+                <span className="h-full">
+                    <Link href="/" className="h-full w-full flex items-center gap-2">
+                        <img src="https://placeholder.com/250x250" className="h-full"  alt="" />
+                        <span className="text-primary txt-7xl">مكتبة الفردوس</span>
+                    </Link>
+                </span>
+                <span className="txt-white">
+                    <Link href="/">المكتبة</Link>
+                </span>
+                <span className="txt-white">
+                    <Link href="/">المكتبة</Link>
+                </span>
                 <span className="txt-white">
                     <Link href="/">المكتبة</Link>
                 </span>
