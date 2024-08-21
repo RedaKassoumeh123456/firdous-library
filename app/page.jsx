015c54ca77fafee805ff4787537829c06be7b93e
MaenGit <MaenGithub@gmail.com>
2024-08-21 16:12:57 +0300

    first books page commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,16 @@
-import React from "react";
 
 const MainPage = () => {
-  return <div>Test</div>;
+  return (
+    <>
+      {/* Search Boxes */}
+      <section className="flex-col md:flex justify-evenly items-center">
+        <form className="p-5 bg-primary rounded-xl w-auto flex items-center justify-between ">
+          <p>ابحث عن اسم كتاب :</p>
+          <imput type="text"  id="book-name" className=""/>
+        </form>
+      </section>
+    </>
+  )
 };
 
 export default MainPage;
